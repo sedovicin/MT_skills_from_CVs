3f8fd75b1174a3088ac2e55ffb95a488269103c8
sdovicin <sebastian.dovicin@fer.hr>
2020-05-22 12:08:05 +0200

    step 4: POS tagging

---
+++ POSTagger.py
@@ -0,0 +1,11 @@
+import nltk
+
+
+def tag_parts_of_speech(sentence_tokens: list):
+	"""Analyses words in sentence and tags each word with part-of-speech label"""
+	return nltk.pos_tag(sentence_tokens)
+
+
+def run(sentences_tokens: list):
+	"""Tags each word in each sentence with a tag that represents a part-of-speech label."""
+	return nltk.pos_tag_sents(sentences_tokens)
---
+++ Tests/test_POSTagger.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+import POSTagger as post
+
+
+class Test(TestCase):
+	def test_tag_parts_of_speech(self):
+		self.assertEqual(SENTENCE_ONE_RES, post.tag_parts_of_speech(SENTENCE_ONE))
+
+	def test_run(self):
+		self.assertEqual([SENTENCE_ONE_RES, SENTENCE_TWO_RES], post.run([SENTENCE_ONE, SENTENCE_TWO]))
+
+
+SENTENCE_ONE = ["This", "is", "a", "test", "sentence", "."]
+SENTENCE_ONE_RES = [('This', 'DT'), ('is', 'VBZ'), ('a', 'DT'), ('test', 'NN'), ('sentence', 'NN'), ('.', '.')]
+SENTENCE_TWO = ["business", "proposition"]
+SENTENCE_TWO_RES = [('business', 'NN'), ('proposition', 'NN')]
