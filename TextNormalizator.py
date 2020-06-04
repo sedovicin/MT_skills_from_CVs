7247e983c07263ee9ebf9817bb31d3e94712619a
sdovicin <sebastian.dovicin@fer.hr>
2020-06-04 10:28:23 +0200

    turned into class; refactoring; added tests; small fixes and changes

--- TextNormalizator.py
+++ TextNormalizator.py
@@ -1,31 +1,46 @@
 from nltk.stem import PorterStemmer
-
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet
 
-stemmer = PorterStemmer();
 
+class WordTextNormalizator:
+	"""Normalizes given words. This can be done using stemming or lemmatization."""
+	def __init__(self):
+		self.stemmer = PorterStemmer()
+		self.lemmatizer = WordNetLemmatizer()
 
-def stem(word: str):
-	return stemmer.stem(word)
+	def stem(self, word):
+		"""Stems word.
 
+		:type word: str"""
+		return self.stemmer.stem(word)
 
-lemmatizer = WordNetLemmatizer()
+	def lemmatize_word(self, word_pos):
+		"""Lemmatizes word. Word must be passed as a tuple, where left value is the word itself,
+		and second value is word's POS tag.
 
+		:type word_pos: tuple"""
+		try:
+			return self.lemmatizer.lemmatize(word_pos[0], self.__transform_pos_for_lemma(word_pos[1]))
+		except KeyError:
+			return self.lemmatizer.lemmatize(word_pos[0])
 
-def lemmatize(word_pos: tuple):
-	try:
-		result = lemmatizer.lemmatize(word_pos[0], transform_pos_for_lemma(word_pos[1]))
-	except KeyError:
-		result = lemmatizer.lemmatize(word_pos[0])
-	return result
+	def lemmatize_sentence(self, sentence):
+		"""Lemmatizes every word in a sentence. See lemmatize_word.
 
+		:type sentence: list[tuple]
+		"""
+		lemmatized_sentence = list()
+		for word in sentence:
+			lemmatized_sentence.append(self.lemmatize_word(word))
+		return lemmatized_sentence
 
-def transform_pos_for_lemma(pos_tag: str):
-	tags = {
-		"ADJ": wordnet.ADJ,
-		"ADV": wordnet.ADV,
-		"NOUN": wordnet.NOUN,
-		"VERB": wordnet.VERB
-	}
-	return tags.get(pos_tag, wordnet.NOUN)
+	@staticmethod
+	def __transform_pos_for_lemma(pos_tag: str):
+		tags = {
+			"ADJ": wordnet.ADJ,
+			"ADV": wordnet.ADV,
+			"NOUN": wordnet.NOUN,
+			"VERB": wordnet.VERB
+		}
+		return tags.get(pos_tag)
