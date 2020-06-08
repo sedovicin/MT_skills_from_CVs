74e484856f58af13bfbe7fbe67412399c4740f43
sdovicin <sebastian.dovicin@fer.hr>
2020-06-08 11:20:47 +0200

    added gitignore from master; small refactoring; updated docstring

--- SentenceTokenizator.py
+++ SentenceTokenizator.py
@@ -1,16 +1,31 @@
 import nltk
 
 
-def tokenize_sentence(sentence: str):
-	"""Segments each sentence into tokens (words, punctuation etc.).
-	Tokens remain in order of appearance in sentence, ie. 'Hello World!' gets
-	segmented into ['Hello', 'World', '!']."""
+def tokenize_sentence(sentence):
+	"""
+	Segments each sentence into tokens (words, punctuation etc.).
+	Tokens remain in order of appearance in sentence, for example: 'Hello World!' gets
+	segmented into ['Hello', 'World', '!'].
+
+	:param sentence: sentence to be processed
+	:type sentence: str
+	:return: sentence as list of words
+	:rtype: list[str]
+	"""
 	return nltk.word_tokenize(sentence)
 
 
-def run(sentences: list):
-	"""Takes list of sentences and segments each sentence into tokens (words, punctuation etc.).
-	Tokens are not mixed together, but separated for each sentence individually."""
+def tokenize_sentences(sentences):
+	"""
+	Takes list of sentences and segments each sentence into tokens (words, punctuation etc.).
+	Tokens are not mixed together, but separated for each sentence individually. In other words,
+	every sentence becomes a list of words.
+
+	:param sentences: list of sentences to be processed
+	:type sentences: list[str]
+	:return: list of tokenized sentences
+	:rtype: list[list[str]]
+	"""
 	sentences_tokens = []
 	for sentence in sentences:
 		sentences_tokens.append(tokenize_sentence(sentence))
