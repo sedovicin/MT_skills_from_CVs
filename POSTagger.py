385287c790c78f93a56a586b9d7615afc79f85f4
sdovicin <sebastian.dovicin@fer.hr>
2020-06-08 11:53:58 +0200

    added gitignore from master; small refactoring; updated docstring

--- POSTagger.py
+++ POSTagger.py
@@ -1,11 +1,25 @@
 import nltk
 
 
-def tag_parts_of_speech(sentence_tokens: list):
-	"""Analyses words in sentence and tags each word with part-of-speech label"""
-	return nltk.pos_tag(sentence_tokens)
+def tag_pos(sentence_tokens):
+	"""
+	Analyses words in sentence and tags each word with part-of-speech label. Uses universal tagset.
+
+	:param sentence_tokens: list of tokens (words) to be processed
+	:type sentence_tokens: list[str]
+	:return: list of tuples (word, pos_tag)
+	:rtype: list[tuple[str,str]]
+	"""
+	return nltk.pos_tag(sentence_tokens, tagset='universal')
 
 
-def run(sentences_tokens: list):
-	"""Tags each word in each sentence with a tag that represents a part-of-speech label."""
-	return nltk.pos_tag_sents(sentences_tokens)
+def tag_pos_sentences(sentences_tokens):
+	"""
+	Tags each word in each sentence with a tag that represents a part-of-speech label.
+
+	:param sentences_tokens: list of sentences (list of tokens (words)) to be processed
+	:type sentences_tokens: list[list[str]]
+	:return: list of list of tuples (word, pos_tag)
+	:rtype: list[list[tuple[str,str]]]
+	"""
+	return nltk.pos_tag_sents(sentences_tokens, tagset='universal')
--- Tests/test_POSTagger.py
+++ Tests/test_POSTagger.py
@@ -4,10 +4,10 @@ import POSTagger as post
 
 class Test(TestCase):
 	def test_tag_parts_of_speech(self):
-		self.assertEqual(SENTENCE_ONE_RES, post.tag_parts_of_speech(SENTENCE_ONE))
+		self.assertEqual(SENTENCE_ONE_RES, post.tag_pos(SENTENCE_ONE))
 
 	def test_run(self):
-		self.assertEqual([SENTENCE_ONE_RES, SENTENCE_TWO_RES], post.run([SENTENCE_ONE, SENTENCE_TWO]))
+		self.assertEqual([SENTENCE_ONE_RES, SENTENCE_TWO_RES], post.tag_pos_sentences([SENTENCE_ONE, SENTENCE_TWO]))
 
 
 SENTENCE_ONE = ["This", "is", "a", "test", "sentence", "."]
