c47776c73a382a9218cbf14813151b2ba3d7e112
sdovicin <sebastian.dovicin@fer.hr>
2020-06-09 17:31:45 +0200

    several fixes; updated docstring; needs generating y_train

--- Tests/test_categorisation.py
+++ Tests/test_categorisation.py
@@ -5,21 +5,23 @@ import json
 
 class MyTestCase(unittest.TestCase):
 	def test_something(self):
-		x_train = []
-		y_train = []
-		with open('../dataset.json', 'r', encoding='utf8') as fp:
-			file = json.load(fp)
-			for item in file.items():
-				x_train.append(item[0])
-				y_train.append(item[1])
 
-		x_test = ["C++", "Marko", "path", "ball", "machine"]
-		y_test = [1,0,0,0,1]
-		categorisator = CategorisatorNN('corpus.json')
+		with open('../corpus_train.json', 'r', encoding='utf8') as fp:
+			x_train = json.load(fp)
+		y_train = list()
+		x_test = [
+			["I'm", "good", "at", "C++"],
+			["Marko", "likes", "Linux"],
+			["Throw", "me", "a", "ball"],
+			["Machine", "learning", "is", "new"]
+		]
+		y_test = [[1, 0, 0, 1], [0, 0, 1], [0, 0, 0, 0], [1, 1, 0, 0]]
+		categorisator = CategorisatorNN('corpus_train.json')
 
 		categorisator.train(x_train, y_train)
-		categorisator.evaluate(x_test, y_test)
-		categorisator.predict(x_test)
+		result = categorisator.evaluate(x_test, y_test)
+		print(result)
+		# categorisator.predict(x_test)
 
 
 if __name__ == '__main__':
--- categorisators.py
+++ categorisators.py
@@ -7,19 +7,24 @@ from keras.models import Model
 
 def y_to_one_hot(y, categories_count):
 	"""
-	Turns regular y (categories) to its one-hot equivalent.
+	Turns target value (categories) to its one-hot equivalent.
 
 	:param y: y to be transformed
-	:type y:
-	:type categories_count: int
+	:type y: list[list[int]]
 	:param categories_count: amount of categories available
-	:return: one-hot equivalent of y, a 2D NumPy array
+	:type categories_count: int
+	:return: one-hot equivalent of y, a NumPy array
 	:rtype: numpy.ndarray
 	"""
-	one_hot = np.zeros((len(y), categories_count))
+	one_hot = np.zeros(len(y))
 	i = 0
-	for value in y:
-		one_hot[i][value] = 1
+	for sentence in y:
+		one_hot_sent = np.zeros((len(sentence), categories_count))
+		j = 0
+		for value in sentence:
+			one_hot_sent[j][value] = 1
+			j += 1
+		one_hot[i] = one_hot_sent
 		i += 1
 	return one_hot
 
@@ -33,45 +38,56 @@ class CategorisatorNN(object):
 		self.x_train = None
 		self.y_train = None
 		self.word2vec = vectorizators.get_word2vec(word2vec_path='word2vec.obj', corpus_path=corpus)
-		self.embedding_matrix = None
-		self.model = None
-		self.tokenizer = Tokenizer()
+		self.model = self.__create_model()
 
-	def train(self, x_train, y_train):
-		self.y_train = y_train
+	def train(self, x_train, y_train, batch_size=None, epochs=1):
+		"""
+		Takes the input and fits it to model.
 
-		self.model = self.create_model()
+		:param x_train: list of sentences. Sentence must be given as list of tokens (words).
+		:type x_train: list[list[str]]
+		:param y_train: same structure as x_train, but instead of word, must contain target data (category)
+		:type y_train: list[list[int]]
+		:param batch_size: Number of samples per gradient update. If unspecified, batch_size will default to 32.
+		:type batch_size: int
+		:param epochs: Number of epochs to train the model.
+		:type epochs: int
+		"""
 		print("Creating x_train...")
-		self.x_train = self.words_to_word2vec_vectors(x_train)
+		self.x_train = self.sentences_to_word2vec_vectors(x_train)
 		print("Creating y_train...")
-		self.y_train = y_to_one_hot(self.y_train, 2)
+		self.y_train = y_to_one_hot(y_train, 2)
 		print("Fitting model...")
 		self.model.fit(
 			self.x_train.reshape((self.x_train.shape[0], 1, self.x_train.shape[1])),
 			self.y_train,
-			batch_size=5000,
-			epochs=10,
+			batch_size=batch_size,
+			epochs=epochs,
 			verbose=1)
 
 	def evaluate(self, x_test, y_test):
 		"""
-		Must give the sentences!
-		:param x_test:
-		:param y_test:
-		:return:
+		Evaluates the model by predicting output and comparing it with actual one.
+
+		:param x_test: text input set
+		:type x_test: list[list[str]]
+		:param y_test: actual values of input
+		:type y_test: list[list[int]]
+		:return: values for loss and accuracy as tuple
+		:rtype: tuple[float, float]
 		"""
-		x_test_seq = self.words_to_word2vec_vectors(x_test)
+		x_test_seq = self.sentences_to_word2vec_vectors(x_test)
 		y_test_seq = y_to_one_hot(y_test, 2)
-		results = self.model.evaluate(
+		return self.model.evaluate(
 			x_test_seq.reshape((x_test_seq.shape[0], 1, x_test_seq.shape[1])),
 			y_test_seq, verbose=1)
-		print("loss, acc", results)
 
 	def predict(self, x):
 		"""
-		Must give the sentences!
-		:param x:
-		:return:
+		Predicts output for given input
+
+		:param x: input to be processed
+		:return: output
 		"""
 		x_test_seq = self.words_to_word2vec_vectors(x)
 		results = self.model.predict(
@@ -80,22 +96,35 @@ class CategorisatorNN(object):
 		print(results)
 
 	def sentences_to_word2vec_vectors(self, sentences):
-		vectors_all = np.array((None, 100))
+		"""
+		Creates list of vectors for all the words from given sentences.
+		Vectors for words are fetched from Word2Vec, or set to 0 (all the values) if
+		Word2Vec does not contain the processed word.
+
+		:param sentences: list of sentences to be processed
+		:type sentences: list[list[str]]
+		:return: arrays containing all vectors
+		:rtype: numpy.ndarray
+		"""
+		print("Transforming all words to vectors...")
+		vectors_all = np.array((None, self.word2vec.wv.vector_size))
 		for sentence in sentences:
 			vectors = self.words_to_word2vec_vectors(sentence)
 			np.concatenate((vectors_all, vectors), out=vectors_all)
+		print("Finished transforming words to vectors.")
+		return vectors_all
 
 	def words_to_word2vec_vectors(self, words):
-		"""Creates list of vectors for given words.
+		"""
+		Creates list of vectors for given words.
 		Vectors for words are fetched from Word2Vec, or set to 0 (all the values) if
 		Word2Vec does not contain the processed word.
 
 		:param words: list of words to be processed
-		:type words: list
+		:type words: list[str]
 		:return: new array containing vectors
 		:rtype: numpy.ndarray
 		"""
-		print("Turning words to vectors...")
 		vectors = np.zeros((len(words), self.word2vec.wv.vector_size))
 		index = 0
 		for word in words:
@@ -105,10 +134,20 @@ class CategorisatorNN(object):
 				vector = self.create_vector_for_oov_word(words, index)
 			vectors[index] = vector
 			index += 1
-		print("Finished turning words to vectors.")
 		return vectors
 
 	def create_vector_for_oov_word(self, words, index):
+		"""
+		Creates vector for out-of-vocabulary word.
+		Uses context (rest of the sentence) of the word to create best matching vector possible.
+
+		:param words: sentence containing context and the word
+		:type words: list[str]
+		:param index: index of the word to be processed
+		:type index: int
+		:return: vector for word. If all the context words are out-of-vocabulary for the current model, returns 0-vector.
+		:rtype: numpy.ndarray
+		"""
 		edited = words.copy()
 		del edited[index]
 		predicted_similar_words = self.word2vec.predict_output_word(edited)
@@ -126,7 +165,13 @@ class CategorisatorNN(object):
 			new_vector += (word_vector * word[1] * factor)
 		return new_vector
 
-	def create_model(self):
+	def __create_model(self):
+		"""
+		Creates model.
+
+		:return: new model
+		:rtype: keras.models.Model
+		"""
 		print("Creating model...")
 		model_input = Input(shape=(None, self.word2vec.vector_size))
 
