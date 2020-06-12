4217057d4dd51c35faec65ae81c4a3f20135e878
sdovicin <sebastian.dovicin@fer.hr>
2020-06-12 11:31:54 +0200

    works! added training in batches, every batch is one CV; needs refactoring and optimization

--- categorisator_pc.py
+++ categorisator_pc.py
@@ -3,6 +3,7 @@ from keras.utils import Sequence
 import nn_input_data_mgmt as nn_mgmt
 import dataset_corpus_mgmt as dc_mgmt
 import numpy as np
+from keras.preprocessing.sequence import pad_sequences
 
 class DataGenerator(Sequence):
 
@@ -86,7 +87,81 @@ class DataGenerator(Sequence):
 	def __len__(self):
 		return 1
 
-from keras.layers import Input, Dense, LSTM, concatenate
+import vectorizators
+word2vec = vectorizators.get_word2vec(word2vec_path='word2vec.obj', corpus_path='corpus_train.json')
+def get_batch(index):
+
+	categories = dict()
+
+	sentences_skills = dc_mgmt.file_to_tokens('cv_extracted/skills/%s_skills.txt' % index)
+	for sentence in sentences_skills:
+		for word in sentence:
+			categories[word] = 1
+	cv_phrases = nn_mgmt.get_phrases('cv_extracted/cvs/%s_cv.txt' % index, categories)
+	y = list()
+	pre_list = list()
+	phr_list = list()
+	post_list = list()
+	i = 0
+	for sent in cv_phrases:
+		for phrase in sent:
+			pre = list()
+			for word in phrase.pre_phrase_context:
+				pre.append(word2vec.wv.get_vector(word))
+			phr = list()
+			for word in phrase.phrase:
+				phr.append(word2vec.wv.get_vector(word))
+			post = list()
+			for word in phrase.post_phrase_context:
+				post.append(word2vec.wv.get_vector(word))
+			if len(pre) == 0:
+				pre = np.zeros((1, word2vec.wv.vector_size)).tolist()
+			# else:
+			# 	pre = np.array(pre)
+			# phr = np.array(phr)
+			if len(post) == 0:
+				post = np.zeros((1, word2vec.wv.vector_size)).tolist()
+			# else:
+			# 	post = np.array(post)
+
+			# print(np.shape(pre))
+			# print(np.shape(phr))
+			# print(np.shape(post))
+			# print("")
+			# pre = [np.array(pre)]
+			# phr = [np.array(phr)]
+			# post = [np.array(post)]
+			# all_together = [pre, phr, post]
+
+			pre_list.append(pre)
+			phr_list.append(phr)
+			post_list.append(post)
+
+			# print(np.shape(pre))
+			# print(np.shape(phr))
+			# print(np.shape(post))
+			# print("")
+			# x.append(all_together)
+			y_entry = np.zeros(2)
+
+			y_entry[phrase.category] = 1
+
+			y.append(y_entry)
+			i+=1
+			# if i == 2:
+			# 	return np.array(pre_list), np.array(phr_list), np.array(post_list), np.array(y)
+	# print(type(x))
+	# print(np.shape(x))
+	# print(type(y))
+	# print(np.shape(y))
+	return \
+		pad_sequences(np.array(pre_list), padding='pre'), \
+		pad_sequences(np.array(phr_list)),\
+		pad_sequences(np.array(post_list), padding='post'), \
+		np.array(y)
+
+
+from keras.layers import Input, Dense, LSTM, concatenate, Masking
 from keras.models import Model
 import vectorizators
 
@@ -96,22 +171,39 @@ model_input1 = Input(shape=(None, generator.word2vec.vector_size))
 model_input2 = Input(shape=(None, generator.word2vec.vector_size))
 model_input3 = Input(shape=(None, generator.word2vec.vector_size))
 
-lstm1 = LSTM(256)(model_input1)
-lstm2 = LSTM(256)(model_input2)
-lstm3 = LSTM(256)(model_input3)
+masking1 = Masking()(model_input1)
+masking2 = Masking()(model_input2)
+masking3 = Masking()(model_input3)
+
+lstm1 = LSTM(256)(masking1)
+lstm2 = LSTM(256)(masking2)
+lstm3 = LSTM(256)(masking3)
 
 dense1 = Dense(128, activation='relu')(lstm1)
 dense2 = Dense(128, activation='relu')(lstm2)
 dense3 = Dense(128, activation='relu')(lstm3)
+
 x = concatenate([dense1, dense2, dense3])
+
 dense = Dense(64, activation='relu')(x)
 dense = Dense(16, activation='relu')(dense)
 main_output = Dense(2, activation='softmax')(dense)
 
 model = Model(inputs=[model_input1, model_input2, model_input3], outputs=main_output)
-
+# model = Model(inputs=model_input1, outputs=main_output)
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 print(model.summary())
 
-model.fit_generator(generator)
+for i in range(1, 100):
+	pr, ph, po, y = get_batch(i)
+	# print(np.shape(pr))
+	# print(np.shape(ph))
+	# print(np.shape(po))
+	# print(np.shape(y))
+	# print("")
+	print(i)
+	model.train_on_batch(x=[pr, ph, po], y=y, reset_metrics=False)
+# model.fit_generator(generator)
+pr, ph, po, y = get_batch(101)
+print(model.evaluate(x=[pr, ph, po], y=y))
