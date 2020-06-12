5b123107b538aa2eafadd3a7e0a54a3b6793b520
sdovicin <sebastian.dovicin@fer.hr>
2020-06-12 13:44:18 +0200

    refactored and optimized performance

--- categorisator_pc.py
+++ categorisator_pc.py
@@ -8,7 +8,7 @@ from keras.preprocessing.sequence import pad_sequences
 import vectorizators
 
 
-word2vec = vectorizators.get_word2vec(word2vec_path='word2vec.obj', corpus_path='corpus_train.json')
+# word2vec = vectorizators.get_word2vec(word2vec_path='word2vec.obj', corpus_path='corpus_train.json')
 def get_batch(index):
 
 	categories = dict()
@@ -59,36 +59,69 @@ from keras.layers import Input, Dense, LSTM, concatenate, Masking
 from keras.models import Model
 
 
-model_input1 = Input(shape=(None, word2vec.vector_size))
-model_input2 = Input(shape=(None, word2vec.vector_size))
-model_input3 = Input(shape=(None, word2vec.vector_size))
 
-masking1 = Masking()(model_input1)
-masking2 = Masking()(model_input2)
-masking3 = Masking()(model_input3)
 
-lstm1 = LSTM(256)(masking1)
-lstm2 = LSTM(256)(masking2)
-lstm3 = LSTM(256)(masking3)
+# for i in range(1, 100):
+# 	pr, ph, po, y = get_batch(i)
+# 	print(i)
+# 	model.train_on_batch(x=[pr, ph, po], y=y, reset_metrics=False)
+# pr, ph, po, y = get_batch(101)
+# print(model.evaluate(x=[pr, ph, po], y=y))
 
-dense1 = Dense(128, activation='relu')(lstm1)
-dense2 = Dense(128, activation='relu')(lstm2)
-dense3 = Dense(128, activation='relu')(lstm3)
+class PhraseContextCategorisator():
+	def __init__(self, vector_size):
+		self.vector_size = vector_size
+		self.model = self.__create_model()
+		print(self.model.summary())
 
-x = concatenate([dense1, dense2, dense3])
+	def __create_model(self):
+		model_input1 = Input(shape=(None, self.vector_size))
+		model_input2 = Input(shape=(None, self.vector_size))
+		model_input3 = Input(shape=(None, self.vector_size))
 
-dense = Dense(64, activation='relu')(x)
-dense = Dense(16, activation='relu')(dense)
-main_output = Dense(2, activation='softmax')(dense)
+		masking1 = Masking()(model_input1)
+		masking2 = Masking()(model_input2)
+		masking3 = Masking()(model_input3)
 
-model = Model(inputs=[model_input1, model_input2, model_input3], outputs=main_output)
+		lstm1 = LSTM(256)(masking1)
+		lstm2 = LSTM(256)(masking2)
+		lstm3 = LSTM(256)(masking3)
 
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-print(model.summary())
+		dense1 = Dense(128, activation='relu')(lstm1)
+		dense2 = Dense(128, activation='relu')(lstm2)
+		dense3 = Dense(128, activation='relu')(lstm3)
 
-for i in range(1, 100):
-	pr, ph, po, y = get_batch(i)
-	print(i)
-	model.train_on_batch(x=[pr, ph, po], y=y, reset_metrics=False)
-pr, ph, po, y = get_batch(101)
-print(model.evaluate(x=[pr, ph, po], y=y))
+		x = concatenate([dense1, dense2, dense3])
+
+		dense = Dense(64, activation='relu')(x)
+		dense = Dense(16, activation='relu')(dense)
+		main_output = Dense(2, activation='softmax')(dense)
+
+		model = Model(inputs=[model_input1, model_input2, model_input3], outputs=main_output)
+
+		model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+		return model
+
+	def train(self, batch_size):
+		"""
+
+		:param batch_size: amount of files to be processed in batch
+		:return:
+		"""
+		generator = nn_mgmt.SampleGenerator(corpus_path='corpus_train.json', dataset_path='dataset_train.json')
+		for i in range(1, 10001, batch_size):
+			print("Training from %s to %s..." %(i, i+batch_size))
+			pre, phr, post, y = generator.get_batch_x_y(i, i+batch_size)
+			pre = pad_sequences(pre)
+			phr = pad_sequences(phr)
+			post = pad_sequences(post)
+			self.model.train_on_batch(x=[pre, phr, post], y=y)
+
+def main():
+	categorisator = PhraseContextCategorisator(100)
+	categorisator.train(50)
+
+
+if __name__ == "__main__":
+	main()
--- nn_input_data_mgmt.py
+++ nn_input_data_mgmt.py
@@ -2,6 +2,7 @@ import dataset_corpus_mgmt as dc_mgmt
 import POSTagger
 import nltk
 from nltk.tree import Tree
+import numpy as np
 
 
 class PhraseParser:
@@ -149,6 +150,118 @@ def create_sets(start, end):
 		cv_phrases = get_phrases('cv_extracted/cvs/%s_cv.txt' % (i+1), categories)
 		all_phrases.extend(cv_phrases)
 
+
+import vectorizators
+
+
+class SampleGenerator:
+	def __init__(self, corpus_path, dataset_path):
+		self.vectorizator = vectorizators.get_word2vec(word2vec_path='word2vec.obj', corpus_path=corpus_path)
+		self.parser = PhraseParser()
+
+	def get_batch_x_y(self, begin, end):
+		"""
+		Creates samples and its targets from files indexed begin index (incl.) to end index (excl.)
+		:param begin:
+		:param end:
+		:return:
+		"""
+		pre_contexts = list()
+		phrases = list()
+		post_contexts = list()
+		ys = list()
+		for i in range(begin, end):
+			cv_sentences = dc_mgmt.file_to_tokens('cv_extracted/cvs/%s_cv.txt' % i)
+			tags = POSTagger.tag_pos_sentences(cv_sentences)
+			parsed_sents = self.parser.parse_sents(tags)
+
+			categories = dict()
+			sentences_skills = dc_mgmt.file_to_tokens('cv_extracted/skills/%s_skills.txt' % i)
+			for sentence in sentences_skills:
+				for word in sentence:
+					categories[word] = 1
+
+			for sentence in parsed_sents:
+				pre_context, phrase, post_context, y = self.phrases_context_as_vectors(sentence, categories)
+				pre_contexts.extend(pre_context)
+				phrases.extend(phrase)
+				post_contexts.extend(post_context)
+				ys.extend(y)
+		return np.array(pre_contexts), np.array(phrases), np.array(post_contexts), np.array(ys)
+
+	def phrases_context_as_vectors(self, sentence, words):
+		i = 0
+		pre_contexts = list()
+		phrases = list()
+		post_contexts = list()
+		ys = list()
+		sent_with_parsed_tags = sentence.pos()
+		while i < len(sent_with_parsed_tags):
+			# FIND NEXT PHRASE INDEX
+			begin = i
+			while (begin < len(sent_with_parsed_tags)) and (sent_with_parsed_tags[begin][1] != 'PHRASE'):
+				begin += 1
+			if begin >= len(sent_with_parsed_tags):
+				break
+
+			# GET IF FIRST WORD OF PHRASE IS SKILL OR NOT
+			category = words.get(sent_with_parsed_tags[begin][0][0], 0)
+
+			j = begin
+
+			# FIND END OF PHRASE
+			while True:
+				j += 1
+				if (j >= len(sent_with_parsed_tags)) \
+					or (sent_with_parsed_tags[j][1] != 'PHRASE') \
+					or (category != words.get(sent_with_parsed_tags[begin][0][0], 0)):
+					break
+
+			# GET PRE-PHASE CONTEXT
+			pre_phrase_context = list()
+			i = begin - 1
+			while (i >= 0) and ((begin - i) <= 3):
+				try:
+					pre_phrase_context.append(self.vectorizator.wv.get_vector(sent_with_parsed_tags[i][0][0]))
+				except KeyError:
+					pre_phrase_context.append(np.zeros(self.vectorizator.wv.vector_size))
+				# TODO: generate vector!
+				i -= 1
+			pre_phrase_context.reverse()
+
+			# GET POST-PHASE CONTEXT
+			post_phrase_context = list()
+			i = j
+			while (i < len(sent_with_parsed_tags)) and ((i - j) < 3):
+				try:
+					post_phrase_context.append(self.vectorizator.wv.get_vector(sent_with_parsed_tags[i][0][0]))
+				except KeyError:
+					post_phrase_context.append(np.zeros(self.vectorizator.wv.vector_size))
+				# TODO: generate vector!
+				i += 1
+
+			# GET PHRASE
+			phrase = list()
+			i = begin
+			while i < j:
+				try:
+					phrase.append(self.vectorizator.wv.get_vector(sent_with_parsed_tags[i][0][0]))
+				except KeyError:
+					phrase.append(np.zeros(self.vectorizator.wv.vector_size))
+				# TODO: generate vector!
+				i += 1
+
+			pre_contexts.append(np.array(pre_phrase_context))
+			phrases.append(np.array(phrase))
+			post_contexts.append(np.array(post_phrase_context))
+
+			y_entry = np.zeros(2)
+			y_entry[category] = 1
+			ys.append(y_entry)
+
+			# Moving on...
+			i = j
+		return pre_contexts, phrases, post_contexts, ys
 
 def main():
 	categories = dict()
